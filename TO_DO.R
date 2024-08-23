06ca7716d733e6c800441391974c1fd8587e03e8
Virgil Ion <virgil.ionn@gmail.com>
2024-08-23 16:07:51 +0300

    new site
    push

---
+++ TO_DO.R
@@ -0,0 +1,18 @@
+# logo for website
+# nume website
+# content * ai un set de date care necesita o analiza statistica, citeste mai mult
+# content cere o oferta
+# content exemple 
+# content sectiune servicii 
+# cautare emoji pentru toate sectiunile
+# content analiza datelor si data science
+# content meniu
+# content sectiune de contact
+# content termeni si conditii
+# content coockies
+# content seo si descrieri
+# setare site in limba romana ca limba principala
+# content pentru 10-20 de exemple
+# content blog cu afilieri
+# content sectiune cursuri statistica
+# 
